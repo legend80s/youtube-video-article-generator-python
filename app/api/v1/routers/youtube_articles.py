41c96f1e3324f655a79e56d2f715c753cb001e37
legend80s <liuchzong@qq.com>
2025-12-29 13:56:52 +0800

    fix: remove redudant api prefix

--- app/api/v1/routers/youtube_articles.py
+++ app/api/v1/routers/youtube_articles.py
@@ -13,19 +13,19 @@ from app.lib.models.articles import ArticleFromTranscript, ArticleFromYoutubeUrl
 router = APIRouter(prefix="/youtube-articles", tags=["youtube-articles"])
 
 
-@router.post("/api/youtube-articles")
+@router.post("")
 async def search_article(
     item: Item | ItemWithTranscript,
 ) -> ArticleFromTranscript | ArticleFromYoutubeUrl:
     return search_article(item)
 
 
-@router.post("/api/youtube-articles/generate")
+@router.post("/generate")
 async def generate_route(item: Item | ItemWithTranscript) -> dict:
     return {"article": await generate(item)}
 
 
-@router.post("/api/youtube-articles/generate_stream")
+@router.post("/generate_stream")
 async def generate_stream_route(item: Item | ItemWithTranscript):
     print(f"{item=}")
     return StreamingResponse(
@@ -40,9 +40,8 @@ async def generate_stream_route(item: Item | ItemWithTranscript):
     )
 
 
-# INFO:     127.0.0.1:53070 - "OPTIONS /api/youtube-articles/generate_stream HTTP/1.1" 405 Method Not Allowed
 # 为现有路由添加OPTIONS处理器
-@router.options("/api/youtube-articles/generate_stream")
+@router.options("/generate_stream")
 async def handle_options():
     """处理OPTIONS预检请求"""
     return JSONResponse(
@@ -56,7 +55,7 @@ async def handle_options():
     )
 
 
-# @router.get("/api/youtube-articles/{id}")
+# @router.get("/{id}")
 # async def read_youtube_article(id: int, session: SessionDep) -> Hero:
 #     hero = await session.get(Hero, id)
 #     if not hero:
